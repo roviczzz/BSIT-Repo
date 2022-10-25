326acc614d3e2e9d0f20cd4db49a5eeab67ddb74
roviczzz <rovic_rodriguez@yahoo.com>
2022-10-25 18:32:04 +0800

    dialogBoxReply

---
+++ firstSem/src/dialogBoxReply.java
@@ -0,0 +1,12 @@
+
+import javax.swing.*;
+
+public class dialogBoxReply {
+
+    public static void main(String[] arg){
+
+        JOptionPane.showMessageDialog(null, "DONE WATCHING", "Rovic M. Rodriguez",JOptionPane.INFORMATION_MESSAGE);
+
+
+    }
+}
