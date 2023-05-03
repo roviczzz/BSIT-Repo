7462d2895ffba8efbd009cc228b6d876f1643a1e
roviczzz <rovic_rodriguez@yahoo.com>
2023-05-03 12:47:30 +0800

    clock

---
+++ firstYear/secondSem/ClockMethodsRodriguez/ClockRun.java
@@ -0,0 +1,14 @@
+
+package ClockMethodsRodriguez;
+
+public class ClockRun {
+
+    public static void main(String[] args) {
+        Clock myClock = new Clock();
+        myClock.setTime(8, 30, 50);
+        myClock.printTimeFormat(false);
+
+
+    }
+
+}
