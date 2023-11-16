dd0edcd7184812e56cd215871924eb3f96efe6b0
rovic <rovic_rodriguez@yahoo.com>
2023-11-16 09:30:32 +0800

    BinaryTree EA

---
+++ secondYear/firstSem/Node.java
@@ -0,0 +1,16 @@
+public class Node {
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int value){
+
+        this.data = value;
+        this.left = null;
+        this.right = null;
+    }
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
