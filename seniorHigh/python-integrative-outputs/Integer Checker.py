fcba58342158d9f77075ffaacfbb246d41669b9e
rovic <rovic_rodriguez@yahoo.com>
2024-08-26 22:35:45 +0800

    Delete seniorHigh directory

--- seniorHigh/python-integrative-outputs/Integer Checker.py
+++
@@ -1,8 +0,0 @@
-number = int(input('Insert Number: '))
-
-if number == 0:
-    print('The number is zero')
-elif number > 0:
-    print('The number is positive')
-elif number < 0:
-    print('The number is negative')
