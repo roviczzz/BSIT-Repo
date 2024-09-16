957e7d3df2828f373d80dbcbf5d01a39560b2b76
rovic <rovic_rodriguez@yahoo.com>
2024-09-16 22:08:21 +0800

    winform

---
+++ .3rdYr/WinFormXMLs/WinFormApp2_Rodriguez/WinFormApp2_Rodriguez/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace WinFormApp2_Rodriguez
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.listBoxData = new System.Windows.Forms.ListBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonView = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonClearTextbox = new System.Windows.Forms.Button();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.textBoxFirstName = new System.Windows.Forms.TextBox();
+            this.textBoxSex = new System.Windows.Forms.TextBox();
+            this.textBoxDayHired = new System.Windows.Forms.TextBox();
+            this.textBoxSalary = new System.Windows.Forms.TextBox();
+            this.textBoxDepartment = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Employee ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Last Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(65, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "First Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(81, 159);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Sex";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(64, 182);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Date Hired";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(55, 208);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Monthly Salary";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(62, 239);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Department";
+            // 
+            // listBoxData
+            // 
+            this.listBoxData.FormattingEnabled = true;
+            this.listBoxData.Location = new System.Drawing.Point(521, 47);
+            this.listBoxData.Name = "listBoxData";
+            this.listBoxData.Size = new System.Drawing.Size(204, 264);
+            this.listBoxData.TabIndex = 7;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(415, 48);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(100, 47);
+            this.buttonAdd.TabIndex = 8;
+            this.buttonAdd.Text = "ADD";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonView
+            // 
+            this.buttonView.Location = new System.Drawing.Point(415, 102);
+            this.buttonView.Name = "buttonView";
+            this.buttonView.Size = new System.Drawing.Size(100, 47);
+            this.buttonView.TabIndex = 9;
+            this.buttonView.Text = "View";
+            this.buttonView.UseVisualStyleBackColor = true;
+            this.buttonView.Click += new System.EventHandler(this.buttonView_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(415, 156);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(100, 47);
+            this.buttonClear.TabIndex = 10;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(415, 210);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(100, 47);
+            this.buttonDelete.TabIndex = 11;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(415, 264);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(100, 47);
+            this.buttonExit.TabIndex = 12;
+            this.buttonExit.Text = "EXIT";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // buttonClearTextbox
+            // 
+            this.buttonClearTextbox.Location = new System.Drawing.Point(146, 266);
+            this.buttonClearTextbox.Name = "buttonClearTextbox";
+            this.buttonClearTextbox.Size = new System.Drawing.Size(112, 41);
+            this.buttonClearTextbox.TabIndex = 13;
+            this.buttonClearTextbox.Text = "Clear Textboxes";
+            this.buttonClearTextbox.UseVisualStyleBackColor = true;
+            this.buttonClearTextbox.Click += new System.EventHandler(this.buttonClearTextbox_Click);
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Location = new System.Drawing.Point(146, 40);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(112, 20);
+            this.textBoxID.TabIndex = 14;
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Location = new System.Drawing.Point(146, 73);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(112, 20);
+            this.textBoxLastName.TabIndex = 15;
+            // 
+            // textBoxFirstName
+            // 
+            this.textBoxFirstName.Location = new System.Drawing.Point(146, 110);
+            this.textBoxFirstName.Name = "textBoxFirstName";
+            this.textBoxFirstName.Size = new System.Drawing.Size(112, 20);
+            this.textBoxFirstName.TabIndex = 16;
+            // 
+            // textBoxSex
+            // 
+            this.textBoxSex.Location = new System.Drawing.Point(146, 152);
+            this.textBoxSex.Name = "textBoxSex";
+            this.textBoxSex.Size = new System.Drawing.Size(112, 20);
+            this.textBoxSex.TabIndex = 17;
+            // 
+            // textBoxDayHired
+            // 
+            this.textBoxDayHired.Location = new System.Drawing.Point(146, 178);
+            this.textBoxDayHired.Name = "textBoxDayHired";
+            this.textBoxDayHired.Size = new System.Drawing.Size(112, 20);
+            this.textBoxDayHired.TabIndex = 18;
+            // 
+            // textBoxSalary
+            // 
+            this.textBoxSalary.Location = new System.Drawing.Point(146, 205);
+            this.textBoxSalary.Name = "textBoxSalary";
+            this.textBoxSalary.Size = new System.Drawing.Size(112, 20);
+            this.textBoxSalary.TabIndex = 19;
+            // 
+            // textBoxDepartment
+            // 
+            this.textBoxDepartment.Location = new System.Drawing.Point(146, 236);
+            this.textBoxDepartment.Name = "textBoxDepartment";
+            this.textBoxDepartment.Size = new System.Drawing.Size(112, 20);
+            this.textBoxDepartment.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxDepartment);
+            this.Controls.Add(this.textBoxSalary);
+            this.Controls.Add(this.textBoxDayHired);
+            this.Controls.Add(this.textBoxSex);
+            this.Controls.Add(this.textBoxFirstName);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.textBoxID);
+            this.Controls.Add(this.buttonClearTextbox);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonView);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.listBoxData);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ListBox listBoxData;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonView;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonClearTextbox;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.TextBox textBoxFirstName;
+        private System.Windows.Forms.TextBox textBoxSex;
+        private System.Windows.Forms.TextBox textBoxDayHired;
+        private System.Windows.Forms.TextBox textBoxSalary;
+        private System.Windows.Forms.TextBox textBoxDepartment;
+    }
+}
+
---
+++ .3rdYr/WinFormXMLs/WinFormApp2_Rodriguez/WinFormApp2_Rodriguez/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WinFormApp2_Rodriguez
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonClearTextbox_Click(object sender, EventArgs e)
+        {
+            textBoxID.Text = "";
+            textBoxLastName.Text = "";
+            textBoxFirstName.Text = "";
+            textBoxSalary.Text = "";
+            textBoxSex.Text = "";
+            textBoxDayHired.Text = "";
+            textBoxSalary.Text = "";
+
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            listBoxData.Items.Clear();  // Clears all the items in the ListBox
+        }
+
+        private void buttonView_Click(object sender, EventArgs e)
+        {
+            listBoxData.Items.Clear();  // Clear existing items in the list
+            string xmlFile = "employees.xml";
+
+            if (System.IO.File.Exists(xmlFile))
+            {
+                // Load the XML file
+                XDocument doc = XDocument.Load(xmlFile);
+                XElement employeesElement = doc.Element("Employees");
+
+                foreach (XElement employee in employeesElement.Elements("Employee"))
+                {
+                    string id = employee.Element("ID")?.Value ?? "No ID";
+                    string lastName = employee.Element("LastName")?.Value ?? "No Last Name";
+                    string firstName = employee.Element("FirstName")?.Value ?? "No First Name";
+                    string sex = employee.Element("Sex")?.Value ?? "No Sex";
+                    DateTime dayHired = DateTime.Parse(employee.Element("DayHired")?.Value ?? DateTime.MinValue.ToString());
+                    decimal salaryPerMonth = decimal.Parse(employee.Element("SalaryPerMonth")?.Value ?? "0");
+                    string department = employee.Element("Department")?.Value ?? "No Department";
+
+                    int yearsEmployed = DateTime.Now.Year - dayHired.Year;
+                    if (DateTime.Now < dayHired.AddYears(yearsEmployed)) yearsEmployed--;
+
+
+
+                    decimal incentiveAmount = incentive(yearsEmployed);  // Call the method to get the incentive
+                    decimal totalSalary = salaryPerMonth + incentiveAmount;  // Use the result of the method
+
+                    // do not forget to insert $ sign before the strings in the Add();
+
+                    listBoxData.Items.Add($"ID:{id}"); listBoxData.Items.Add($"Last Name: {lastName}");
+                    listBoxData.Items.Add($"FirstName:{firstName}"); listBoxData.Items.Add($"Sex: {sex}");
+                    listBoxData.Items.Add($"DayHired:{dayHired.ToShortDateString()}"); listBoxData.Items.Add($"Salary Per Month: {salaryPerMonth:C}");
+                    listBoxData.Items.Add($"Department:{department}"); listBoxData.Items.Add($"Years Employed: {yearsEmployed}");
+                    listBoxData.Items.Add($"Incentive:{incentiveAmount:C}"); listBoxData.Items.Add($"Total Salary: {totalSalary:C}");
+                    listBoxData.Items.Add(new string('-', 40));
+                }
+            }
+            else
+            {
+                MessageBox.Show("No XML file found. Please add data first.");
+            }
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            string xmlFile = "employees.xml";
+
+            // Check if the file exists
+            if (System.IO.File.Exists(xmlFile))
+            {
+                // Delete the file
+                System.IO.File.Delete(xmlFile);
+                MessageBox.Show("XML file cleared successfully.");
+
+                // Optionally, clear the ListBox as well
+                listBoxData.Items.Clear();
+            }
+            else
+            {
+                MessageBox.Show("No XML file found.");
+            }
+
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            string id = textBoxID.Text;
+            string lastName = textBoxLastName.Text;
+            string firstName = textBoxFirstName.Text;
+            string sex = textBoxSex.Text;
+            DateTime dayHired;
+            decimal salaryPerMonth;
+            string department = textBoxDepartment.Text;
+
+            if (!DateTime.TryParse(textBoxDayHired.Text, out dayHired))
+            {
+                MessageBox.Show("Please enter a valid Date Hired.");
+                return;
+            }
+
+            if (!decimal.TryParse(textBoxSalary.Text, out salaryPerMonth))
+            {
+                MessageBox.Show("Please enter a valid Salary Per Month.");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(id) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(sex) || string.IsNullOrEmpty(department))
+            {
+                MessageBox.Show("Please enter all fields.");
+                return;
+            }
+
+            // Create an XML document and save the data
+            XElement employee = new XElement("Employee",
+                new XElement("ID", id),
+                new XElement("LastName", lastName),
+                new XElement("FirstName", firstName),
+                new XElement("Sex", sex),
+                new XElement("DayHired", dayHired.ToString("yyyy-MM-dd")),
+                new XElement("SalaryPerMonth", salaryPerMonth),
+                new XElement("Department", department)
+            );
+
+            XDocument doc;
+            string xmlFile = "employees.xml";
+
+            // Check if XML file exists
+            if (System.IO.File.Exists(xmlFile))
+            {
+                doc = XDocument.Load(xmlFile); // Load the existing file
+                doc.Element("Employees").Add(employee); // Add new employee to the root
+            }
+            else
+            {
+                // Create a new XML document if file doesn't exist
+                doc = new XDocument(
+                    new XElement("Employees", employee));
+            }
+
+            doc.Save(xmlFile); // Save the document
+            MessageBox.Show("Employee data saved to XML successfully.");
+        }
+        private decimal incentive(int yearsEmployed)
+        {
+            if (yearsEmployed >= 20)
+            {
+                return 20000m;
+            }
+            else if (yearsEmployed >= 15)
+            {
+                return 10000m;
+            }
+            else if (yearsEmployed >= 10)
+            {
+                return 5000m;
+            }
+            else
+            {
+                return 0m;
+            }
+        }
+    }
+}
