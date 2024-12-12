84abd1fa8d675d9b512817184c5c1a270eacbf01
rovic <rovic_rodriguez@yahoo.com>
2024-12-12 21:44:23 +0800

    Maj Projects

---
+++ .MajProjects/WebApplications/Rodriguez_ScriptSocietyMVC/Rodriguez_ScriptSocietyMVC/Data/Rodriguez_ScriptSocietyMVCContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Rodriguez_ScriptSocietyMVC.Models;
+
+namespace Rodriguez_ScriptSocietyMVC.Data
+{
+    public class Rodriguez_ScriptSocietyMVCContext : DbContext
+    {
+        public Rodriguez_ScriptSocietyMVCContext (DbContextOptions<Rodriguez_ScriptSocietyMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Rodriguez_ScriptSocietyMVC.Models.Inventory> Inventory { get; set; } = default!;
+        public DbSet<Rodriguez_ScriptSocietyMVC.Models.Campaign> Campaign { get; set; } = default!;
+    }
+}
---
+++ .MajProjects/WebApplications/Rodriguez_ScriptSocietyMVC/Rodriguez_ScriptSocietyMVC/Models/Campaign.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Rodriguez_ScriptSocietyMVC.Models
+{
+
+    public class Campaign
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CampaignId { get; set; }
+
+        [Required]
+        [ForeignKey("Inventory")]
+        public int ProductId { get; set; }
+
+        public Inventory? Inventory { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string? Series { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string? Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        public string? Status { get; set; }
+    }
+}
