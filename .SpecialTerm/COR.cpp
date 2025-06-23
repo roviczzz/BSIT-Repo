750d567810747495ba60926abd34d4261fc31442
rovic <rovic_rodriguez@yahoo.com>
2025-06-23 10:04:41 +0800

    Update COR.cpp

---
+++ .SpecialTerm/COR.cpp
@@ -0,0 +1,26 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+int main() {
+    // Write C++ code here
+    std::cout << "STUDENT NO: 202080044\n";
+    std::cout << "STUDENT TYPE: REGULAR\n";
+    std::cout << "NAME: RODRIGUEZ, ROVIC MOJICA\n";
+    std::cout << "PROGRAM CODE: BIT33\n";
+    std::cout << "COLLEGE: CICS\n\n";
+    
+    std::cout << "COURSE CODE \t\tSECTION CODE \t\tCOURSE TITLE \t\t\t\t\t\tNO. OF UNITS\n";
+    std::cout << "S-ITWB344 \t\t\t\tS01 \t\tINTERNET LANGUAGES AND TOOLS 1 LEC \t\t\t2.00\n";
+    std::cout << "S-ITWB344 \t\t\t\tS01 \t\tINTERNET LANGUAGES AND TOOLS 1 LEC \t\t\t2.00\n";
+    std::cout << "S-ITWB344LA \t\t\tS01 \t\tINTERNET LANGUAGES AND TOOLS 1 LAB \t\t\t1.00\n";
+    std::cout << "S-ITPE003 \t\t\t\tS03 \t\tIT PROFESSIONAL ELECTIVE LEC \t\t\t\t2.00\n";
+    std::cout << "S-ITPE003 \t\t\t\tS03 \t\tIT PROFESSIONAL ELECTIVE LEC \t\t\t\t2.00\n";
+    std::cout << "S-ITPE003 \t\t\t\tS03 \t\tIT PROFESSIONAL ELECTIVE LEC \t\t\t\t2.00\n";
+    std::cout << "S-ITPE003LA \t\t\tS03 \t\tIT PROFESSIONAL ELECTIVE LAB \t\t\t\t1.00\n\n";
+    std::cout << "Total Units: 6.00\n";
+    std::cout << "======================================================================================\n";
+
+    
+
+    return 0;
+}
