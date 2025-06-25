084fca08c7b80c6a551496ae4d3ad405c34c75e5
rovic <rovic_rodriguez@yahoo.com>
2025-06-25 10:59:28 +0800

    update operators.cpp

--- .SpecialTerm/Operators.cpp
+++ .SpecialTerm/Operators.cpp
@@ -15,6 +15,7 @@ cout << "Sum: " << a+b+c << "\n";
 cout << "Difference: " << a-b-c << "\n";
 cout << "Product: " << a*b*c << "\n";
 cout << "Quotient: " << a/b/c << "\n";
+cout << "Remainder: " << a%b%c << "\n";
 
 
 return 0;
